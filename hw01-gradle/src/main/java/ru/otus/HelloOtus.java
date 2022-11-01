daa2a1491921e8c70bb0efadea641e026a000fba
Stern_Ritter <fadeef.k@yandex.ru>
2022-11-01 22:40:41 +0300

    fix/rename Main class name

--- hw01-gradle/src/main/java/ru/otus/HelloOtus.java
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -3,7 +3,7 @@ package ru.otus;
 import java.util.List;
 import com.google.common.collect.Lists;
 
-public class Main {
+public class HelloOtus {
     public static void main(String[] args) {
         List<String> fruits = Lists.newArrayList("orange", "banana", "kiwi", "mandarin", "date", "quince");
         fruits.forEach(System.out::println);
